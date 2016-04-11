49f2494a7c1bd779567b1f99ef493180ba2775bb
Notechus <seba0713@gmail.com>
2016-04-11 19:55:36 +0200

    register

--- music/urls.py
+++ music/urls.py
@@ -7,8 +7,13 @@ app_name = 'music'
 urlpatterns = [
     # /music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
     # /music/71/
     url(r'^(?P<id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     # /music/album/add
     url(r'^album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+    # /music/album/2
+    url(r'^album/(?P<id>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+    # /music/album/2/delete/
+    url(r'^album/(?P<id>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
